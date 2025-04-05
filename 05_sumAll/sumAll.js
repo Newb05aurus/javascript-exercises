713f2886c55e92c3df003d0526148ddeb8e42e1a
Newb05aurus <198301838+Newb05aurus@users.noreply.github.com>
2025-04-04 23:26:48 -0400

    Complete foundations javascript-exercises 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+    let sum = 0;
 
+    if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) return `ERROR`; 
+    
+    if (numOne < 0 || numTwo < 0) return `ERROR`;
+
+    if (numOne >= numTwo) {
+        let i = numTwo;
+        for (i; i <= numOne; i++){
+            sum += i;
+        }
+    } else { 
+        let i = numOne;
+        for (i; i <= numTwo; i++){
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
