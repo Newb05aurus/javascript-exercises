d51e8f8993c8d4477d7d09d5a6f9a67bf67bde77
Newb05aurus <198301838+Newb05aurus@users.noreply.github.com>
2025-04-04 22:28:59 -0400

    Complete foundations javascript-exercises 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, ...toRemove) {
+    return givenArray.filter(item => !toRemove.includes(item));
+
 };
 
 // Do not edit below this line
