e9b7959d657301f3be829a281f0554a0d948f696
Newb05aurus <198301838+Newb05aurus@users.noreply.github.com>
2025-04-04 20:26:17 -0400

    Complete Foundations javascript-exercises 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newReversedString = "";
+    for (let i = 0; i < string.length; i++){
+        newReversedString = string[i] + newReversedString;
+    }
+    return newReversedString;
 };
 
 // Do not edit below this line
