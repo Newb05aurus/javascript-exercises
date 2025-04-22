eb9bbcf3f7f04c631c9c9d5a2221e666ed9371c9
Newb05aurus <198301838+Newb05aurus@users.noreply.github.com>
2025-04-22 02:27:35 -0400

    Complete foundations-javascript-exercises-09_Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedString = reverseCleanString(str);
+    return cleanedString === cleanedString.split('').reverse().join('');  // Compare the cleaned string with its reverse
 };
+
+function reverseCleanString(str) {
+    return str
+        .split('')
+        .filter(char => /[a-zA-Z0-9]/.test(char))  // keep only letters and numbers
+        .reverse()
+        .join('')
+        .toLowerCase();
+};
+    
+
 
 // Do not edit below this line
 module.exports = palindromes;
